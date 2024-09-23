304ab07dd9916aa95ee0ebe223375d3bbd7f9c0a
isaksolheim <isak@isak.me>
2024-09-23 09:54:49 +0200

    Create handleInputChange function

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,23 @@ function App() {
   const starterText =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
+  const handleInputChange = () => {
+    if (score === 0) {
+      setStartTime(new Date().getTime());
+    }
+
+    if (starterText.charAt(score) === " ") {
+      setWords(words + 1);
+    }
+
+    setScore(score + 1);
+
+    setText({
+      highlighted: starterText.slice(0, score),
+      default: starterText.slice(score, starterText.length),
+    });
+  };
+
   return (
     <main className="pt-32 bg-black h-screen text-white">
       <div className="relative max-w-2xl m-auto text-xl text-white">
@@ -27,21 +44,7 @@ function App() {
         <input
           type="text"
           className="absolute opacity-0 top-0 left-0 w-full h-full z-0"
-          onChange={() => {
-            if (score === 0) {
-              setStartTime(new Date().getTime());
-            }
-
-            if (starterText.charAt(score) === " ") {
-              setWords(words + 1);
-            }
-
-            setScore(score + 1);
-            setText({
-              highlighted: starterText.slice(0, score),
-              default: starterText.slice(score, starterText.length),
-            });
-          }}
+          onChange={handleInputChange}
         />
 
         {score === 0 ? (
